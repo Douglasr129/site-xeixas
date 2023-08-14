00a45bc5c73101a53dc79c6e44907f895624bd3b
Douglas Rodrigues <douglas.r129@outlook.com>
2023-08-14 17:42:27 -0300

    Edite carousel

--- src/components/carousel/carousel.styles.ts
+++ src/components/carousel/carousel.styles.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 
-type StyleProps = {
-	dark?: boolean;
+type StyleButtonProps = {
+	$active?: boolean;
 };
-export const Container = styled.div<StyleProps>`
-	background-color: yellow;
+export const Container = styled.div`
 	display: flex;
 	flex-direction: row;
 	flex-wrap: nowrap;
@@ -12,39 +11,50 @@ export const Container = styled.div<StyleProps>`
 	width: 100%;
 	margin:0;
 `;
-export const content = styled.div<StyleProps>`
-	background-color: blue;
+export const content = styled.div`
 	height: 100vh;
 	display: flex;
 	width: 100%;
 	flex-direction: column;
 	justify-content: space-between;
-	padding: 1.5rem;
+	padding: ${(props) => props.theme.paddings.small};
 `;
-export const previous = styled.div<StyleProps>`
-	background-color: rgba(0,0,0,0.3);
+export const sideBar = styled.div`
 	display:flex;
-	color: white;
+	color: rgba(0,0,0,0.1);
+	cursor: pointer;
 	font-size: 4rem;
 	width: 5rem;
 	align-items: center;
 	justify-content: center;
+	&:hover {
+		background-color: rgba(0,0,0,0.1);
+		color: white;
+	}
 `;
-export const next = styled.div<StyleProps>`
-	background-color: rgba(0,0,0,0.3);
-	display:flex;
-	color: white;
-	font-size: 4rem;
-	width: 5rem;
-	align-items: center;
-	justify-content: center;
-`;
-export const indexBar = styled.div<StyleProps>`
-	background-color: rgba(0,0,0,0.3);
+export const indexBar = styled.div`
 	width: 100%;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
 `;
-export const Bar = styled.div<StyleProps>`
-	background-color: white;
+export const Bar = styled.div<StyleButtonProps>`
+	background-color: ${(props) =>
+		props.$active ? props.theme.colors.secondary : 'rgba(0,0,0,0.1)'};
 	width: 100%;
-	height: 1.5rem;
+	border-radius: 0.1rem;
+	cursor: pointer;
+	margin-left: 0.4rem;
+	margin-right: 0.4rem;
+	height: 0.4rem;
+	animation: go-back 1s ease-in-out;
+
+	@keyframes go-back {
+  from {
+    transform: translateY(10px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
 `;
--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,21 +1,68 @@
-import { ReactNode } from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import { ReactNode, useState } from 'react';
 import * as Styled from './carousel.styles';
 import { Section } from '../Section';
+import { ArrowLeft } from '@styled-icons/material-outlined/ArrowLeft';
+import { ArrowRight } from '@styled-icons/material-outlined/ArrowRight';
 
 interface carouselProps {
 	children: ReactNode[];
 	sectionId: string;
 }
+const itemsPerPage = 1; // Número de itens por página
 export const carousel = ({ children = [], sectionId }: carouselProps) => {
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const indexOfLastItem = currentPage * itemsPerPage;
+	const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+	const currentItems = children.slice(indexOfFirstItem, indexOfLastItem);
+
+	const totalPages = Math.ceil(children.length / itemsPerPage);
+
+	const handlePageChange = (pageNumber: number) => {
+		if (pageNumber > totalPages) {
+			pageNumber = 1;
+		}
+		setCurrentPage(pageNumber);
+	};
+	const handlePrevius = (pageNumber: number) => {
+		if (pageNumber < 1) {
+			pageNumber = totalPages;
+		}
+		console.log(
+			'Pagina = ' + pageNumber,
+			'Primeira = ' + indexOfFirstItem,
+			'Ultima = ' + indexOfLastItem,
+			'Pagina corrente = ' + currentPage,
+		);
+		setCurrentPage(pageNumber);
+	};
 	return (
 		<Section sectionId={sectionId}>
 			<Styled.Container>
-				<Styled.previous>{'<'}</Styled.previous>
+				<Styled.sideBar onClick={() => handlePrevius(currentPage - 1)}>
+					<ArrowLeft />
+				</Styled.sideBar>
 				<Styled.content>
-					{children[0]}
-					<Styled.indexBar>{'...'}</Styled.indexBar>
+					{currentItems.map((item, index) => (
+						<div key={index}>{item}</div>
+					))}
+
+					<Styled.indexBar>
+						{Array.from({ length: totalPages }, (_, index) => {
+							return (
+								<Styled.Bar
+									key={index}
+									$active={index + 1 == currentPage ? true : false}
+									onClick={() => handlePageChange(index + 1)}
+								></Styled.Bar>
+							);
+						})}
+					</Styled.indexBar>
 				</Styled.content>
-				<Styled.next>{'>'}</Styled.next>
+				<Styled.sideBar onClick={() => handlePageChange(currentPage + 1)}>
+					<ArrowRight />
+				</Styled.sideBar>
 			</Styled.Container>
 		</Section>
 	);
